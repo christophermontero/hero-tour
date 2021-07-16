748cdd09839ebcefc03ea0e3d6e2bd2b9830d4b6
christophermontero <chris.to20@hotmail.com>
2021-07-16 12:25:01 -0500

    Set up code linting

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'max-len': [
+      'error',
+      {
+        'code': 80,
+        'ignoreComments': true
+      }
+    ],
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hero-tour'`, () => {
+  it('should have as title \'hero-tour\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('hero-tour');
